58b2e6a9488f8a584198f77a84b3ec1ec8cbb77c
Andreas Erlandsson <10392013+cited-cub@users.noreply.github.com>
2021-01-05 11:33:35 +0100

    Prepare for production

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-const url = `mongodb://localhost:27017/kicasorganizer`;
+const url = `mongodb+srv://dbuser:pa55w0rd2019@cluster0.2n8ue.mongodb.net/kicasdb?retryWrites=true&w=majority` || `mongodb://localhost:27017/kicasorganizer`;
 let db = null;
 
 export async function connectDB() {
